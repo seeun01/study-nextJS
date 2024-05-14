8920ff6e20f095ce234f070aa3e3d08f51a17f94
seeun <ssky601@gmail.com>
2024-05-14 16:38:12 +0900

    병렬로  fetch하기

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 export const metadata = {
     title: "Home",
 }
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
 
 async function getMovies() {
-    await new Promise((resolve) => setTimeout(resolve, 10000))
-    const response = await fetch(URL);
+    // await new Promise((resolve) => setTimeout(resolve, 1000))
+    const response = await fetch(API_URL);
     const json = await response.json();
     return json;
 }
@@ -16,7 +18,13 @@ export default async function Page() {
 
     return (
         <div>
-            {JSON.stringify(movies)}
+           {movies.map((movie) => (
+            <li key={movie.id}>
+                <Link href={`/movies/${movie.id}`}>
+                    {movie.title}
+                </Link>
+            </li>
+           ))}
         </div>
     )
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,3 +1,18 @@
-export default function MovieDetail({params: {id},} : {params: {id : string};}){
-    return <h1>Movie {id}</h1>
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id: string){
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    const response = await fetch(`${API_URL}/${id}`);
+    return await response.json();
+}
+
+async function getVideos(id:string){
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    const response = await fetch(`${API_URL}/${id}/videos`);
+    return await response.json();
+}
+
+export default async function MovieDetail({params: {id},} : {params: {id : string};}){
+    const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+    return <h1>{movie.title}</h1>
 }
