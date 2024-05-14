fccfa6b92857354d53a8dcae3cc2cf178acee430
seeun <ssky601@gmail.com>
2024-05-14 12:28:56 +0900

    usePathname

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
-export default function Tomato() {
-    return <h1>Hello NextJs!</h1>
+import Navigation from "../components/navigation"
+
+export default function Page() {
+    return (
+        <div>
+            <Navigation />
+            <h1>Hello</h1>
+        </div>
+    )
 }
