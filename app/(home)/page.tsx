cb8d2233977e772cb7047be5968b512ea6f8a73f
seeun <ssky601@gmail.com>
2024-05-14 15:22:31 +0900

    Client Side에서 API와 통신

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,25 @@
-export const metadata = {
-    title: "Home",
-  }
+"use client"
+import { useEffect, useState } from "react"
 
 export default function Page() {
+
+    const [isLoding, setIsLoding] = useState(true);
+    const [movies, setMovies] = useState([]);
+
+    const getMovies = async () => {
+        const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
+        const json = await response.json();
+        setMovies(json);
+        setIsLoding(false);
+    }
+
+    useEffect(() => {
+        getMovies();
+    }, [])
+
     return (
         <div>
-            <h1>Hello</h1>
+            {isLoding ? "Loding..." :JSON.stringify(movies)}
         </div>
     )
 }
