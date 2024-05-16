c291f04fda8a6c02ba5575b0a052bc03cc0e4820
seeun <ssky601@gmail.com>
2024-05-16 11:18:55 +0900

    CSS Modules & Movie Styles

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -3,9 +3,8 @@ import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id:string){
     await new Promise((resolve) => setTimeout(resolve, 3000))
-    throw new Error('something broke...')
-    // const response = await fetch(`${API_URL}/${id}/videos`);
-    // return await response.json();
+    const response = await fetch(`${API_URL}/${id}/videos`);
+    return await response.json();
 }
 
 export default async function MovieVideos({id} : {id:string}){
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,17 +1,20 @@
 "use client"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import styles from "../styles/navigation.module.css"
 
 export default function Navigation(){
     const path = usePathname();
     return (
-        <nav>
-            <li>
-                <Link href="/">Home</Link>{path === "/" ? "🔥" : ""}
-            </li>
-            <li>
-                <Link href="/about-us">About US</Link>{path === "/about-us" ? "🔥" : ""}
-            </li>
+        <nav className={styles.nav}>
+            <ul>
+                <li>
+                    <Link href="/">Home</Link>{path === "/" ? "🔥" : ""}
+                </li>
+                <li>
+                    <Link href="/about-us">About US</Link>{path === "/about-us" ? "🔥" : ""}
+                </li>
+            </ul>
         </nav>
     )
 }
