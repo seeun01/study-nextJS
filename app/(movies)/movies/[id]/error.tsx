02cd97f96b8b7ad036a95eaac60333ee377cad44
seeun <ssky601@gmail.com>
2024-05-16 10:16:35 +0900

    Error Handling

---
+++ app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,5 @@
+"use client"
+
+export default function errorOMG(){
+    return <h1>lol somthing broke...</h1>
+}
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,9 +1,11 @@
+import { error } from "console";
 import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id:string){
     await new Promise((resolve) => setTimeout(resolve, 3000))
-    const response = await fetch(`${API_URL}/${id}/videos`);
-    return await response.json();
+    throw new Error('something broke...')
+    // const response = await fetch(`${API_URL}/${id}/videos`);
+    // return await response.json();
 }
 
 export default async function MovieVideos({id} : {id:string}){
